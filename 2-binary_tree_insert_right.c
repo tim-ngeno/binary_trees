bf5448ea858ff192be7ec69a00b48ca8aa275a4e
Tim Ngeno <timngeno424@gmail.com>
2023-08-29 10:20:11 +0300

    insert a node as the right child - update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,20 +12,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	new->parent = parent;
-	new->n = value;
-	new->left = NULL;
-	new->right = parent->right;
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
 	if (parent->right != NULL)
 	{
+		new->right = parent->right;
 		parent->right->parent = new;
 	}
-
 	parent->right = new;
 
 	return (new);
