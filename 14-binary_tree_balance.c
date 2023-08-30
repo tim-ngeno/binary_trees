becd1f97fe66c3cc46bd3c0ef27d1a53154663d1
Tim Ngeno <timngeno424@gmail.com>
2023-08-30 09:26:19 +0300

    use binary_tree_height to measure height diff

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (left - right);
 }
