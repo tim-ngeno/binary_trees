8405e6d16c188670dfb808bc9069f2e2d35bad4c
Tim Ngeno <timngeno424@gmail.com>
2023-08-30 11:18:24 +0300

    check for perfect binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,28 +1,31 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect(each
- * node has exactly 2 children and all leaf nodes are at same level)
- * @tree: pointer to the root node of the target tree
+ * is_perfect - checks if a binary tree is perfect
+ * @node: current node being checked
+ * @level: the level of the target node
+ * @height: height of the binary tree
  *
- * Return: 1 on success, 0 otherwise
+ * Return: 1 if the binary tree is perfect, 0 otherwise
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int is_perfect(const binary_tree_t *node, int level, int height)
 {
-	if (tree == NULL)
-		return (0);
-
-	if ((tree->left && tree->right) &&
-	    binary_tree_height(tree->left) -
-	    binary_tree_height(tree->right) == 0)
+	if (node == NULL)
 		return (1);
 
-	if (binary_tree_is_perfect(tree->left) &&
-	    binary_tree_is_perfect(tree->right))
-		return (1);
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (level == height);
+	}
+
+	if (node->left != NULL && node->right != NULL)
+	{
+		return ((is_perfect(node->left, level + 1, height)
+			 && (is_perfect(node->right, level + 1, height))));
+	}
 
 	return (0);
-
 }
 
 
@@ -47,4 +50,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return ((left > right) ? left : right);
 
+}
+
+
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect(each
+ * node has exactly 2 children and all leaf nodes are at same level)
+ * @tree: pointer to the root node of the target tree
+ *
+ * Return: 1 on success, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	return (is_perfect(tree, 0, height));
 }
