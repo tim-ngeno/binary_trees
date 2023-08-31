9b34f670de025914ad4879c2bf512be6b2556332
Tim Ngeno <timngeno424@gmail.com>
2023-08-31 22:15:53 +0300

    revert changes

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,26 +9,17 @@
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *new = tree->right;
-	binary_tree_t *temp = new->left;
 
 	if (tree == NULL || tree->right == NULL)
 		return (tree);
 
+	tree->right = new->left;
+
+	if (new->left != NULL)
+		new->left->parent = tree;
 	new->left = tree;
-	tree->right = temp;
-
-	if (temp != NULL)
-		temp->parent = tree;
-	temp = tree->parent;
+	new->parent = tree->parent;
 	tree->parent = new;
-	new->parent = temp;
-
-	if (temp != NULL)
-	{
-		if (temp->left == tree)
-			temp->left = new;
-		temp->right = new;
-	}
 
 	return (new);
 }
